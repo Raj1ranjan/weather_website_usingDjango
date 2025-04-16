d3a2d89404ae054c5220653ce37e7667a12ae8dc
Raj1ranjan <imranjan3339@gmail.com>
2025-04-16 11:54:00 +0530

    Fix: use correct API key environment variable

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$xs9j7!g_ad!s2f#-cc!t#&-wbant=_r+@n%ci*zyh!ca2gd6h')
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
